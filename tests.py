2fffb8932187ed9e3fd99b2e8f4b2275a3c241bb
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-05-14 17:53:45 +0300

    fix: tests in Russian

--- tests.py
+++ tests.py
@@ -5,29 +5,29 @@ from typograf import typografy_text
 
 class TestTypografFunction(unittest.TestCase):
     def test_single_quotes(self):
-        self.assertEqual(typografy_text('correct single \"quotes\"'),
-         'correct single «quotes»')
+        self.assertEqual(typografy_text('КБ "ЙЦУКЕН"'),
+         'КБ «ЙЦУКЕН»')
     def test_double_quotes(self):
-        self.assertEqual(typografy_text('\"Something \"something\"\"'),
-         '«Something \"something\"»')
-    def test_replace_hyphen_with_dashe_in_phone_numbers(self):
+        self.assertEqual(typografy_text('НИИ "КБ "ЙЦУКЕН""'),
+         'НИИ «КБ "ЙЦУКЕН"»')
+    def test_replace_hyphen_with_dash_in_phone_numbers(self):
         self.assertEqual(typografy_text('+7—123—456—78—90'),
          '+7–123–456–78–90')
-    def test_replace_dashes_with_hyphen(self):
-        self.assertEqual(typografy_text('something - something'),
-         'something — something')
+    def test_replace_hyphen_with_dash(self):
+        self.assertEqual(typografy_text('Тире - один из знаков препинания.'),
+         'Тире — один из\u00A0знаков препинания.')
     def test_link_conjunction_word_with_no_break_space(self):
-        self.assertEqual(typografy_text('at work'),
-         'at\u00A0work')
+        self.assertEqual(typografy_text('на работе'),
+         'на\u00A0работе')
     def test_link_numbers_with_following_words_by_no_break_space(self):
-        self.assertEqual(typografy_text('300 monkey'),
-         '300\u00A0monkey')
+        self.assertEqual(typografy_text('300 обезьян'),
+         '300\u00A0обезьян')
     def test_remove_extra_spaces(self):
-        self.assertEqual(typografy_text('remove     extra   spaces'),
-         'remove extra spaces')
+        self.assertEqual(typografy_text('очень   много   пробелов'),
+         'очень много пробелов')
     def test_remove_extra_line_breaks(self):
-        self.assertEqual(typografy_text('something\n\n\nsomething'),
-         'something\nsomething')
+        self.assertEqual(typografy_text('много пустых\n\n\nстрок'),
+         'много пустых\nстрок')
 
 
 if __name__ == "__main__":
